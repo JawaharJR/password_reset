433e9e50bd3ff2c8a7191b3066879bdd9fc8d82b
JawaharJR <107339319+JawaharJR@users.noreply.github.com>
2023-03-01 14:20:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./Components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import CreateAccount from "./Components/CreateAccount";
+import PasswordReset from "./Components/PasswordReset";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="create-account" element={<CreateAccount />} />
+        <Route path="reset-password" element={<PasswordReset />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CreateAccount.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import axios from "axios";
+
+const CreateAccount = () => {
+  const [accountData, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [createStatus, setCreateStatus] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      let response = await axios.post(
+        "https://password-reset-zen.herokuapp.com/create-account",
+        {
+          ...accountData,
+        }
+      );
+      setCreateStatus(response.data.msg);
+      setData({
+        email: "",
+        password: "",
+      });
+      setInterval(() => {
+        setCreateStatus(false);
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="col-xs-12 col-md-5 col-lg-6">
+      <form
+        className="mt-4 p-2 ms-5 me-5"
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <div className="form-floating mb-3 ">
+          <input
+            type="email"
+            value={accountData.email}
+            onChange={(e) => {
+              setData({ ...accountData, email: e.target.value });
+            }}
+            className="form-control"
+            id="floatingInput"
+            required
+          />
+          <label for="floatingInput">Email</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            onChange={(e) => {
+              setData({ ...accountData, password: e.target.value });
+            }}
+            value={accountData.password}
+            className="form-control"
+            id="floatingPassword"
+            required
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+        <button type="submit" className="btn btn-success mt-3">
+          Create
+        </button>
+      </form>
+      {createStatus && (
+        <div class="alert alert-success w-25 ms-5" role="alert">
+          account created!!
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CreateAccount;
